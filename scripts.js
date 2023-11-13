4fb7b6a61266279a2a2d39ab5281f412d3db8cad
Bruno <brunoamagar@campus.monlau.com>
2023-11-13 15:55:41 +0100

    general-updates

--- scripts.js
+++ scripts.js
@@ -1,72 +1,73 @@
-const songs = [{
-    src: '../music/song1.mp3',
-    img: 'https://imgs.search.brave.com/4utU7dd-hTGeLy0FOv2ujN0voiRqcby1h2xuKRsvifw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pMS5z/bmRjZG4uY29tL2Fy/dHdvcmtzLTRkMmEw/Y2NmLWFkYjctNGYz/Yy1hYzg4LWE2MGFm/ZWJmMDg1Yi0wLXQ1/MDB4NTAwLmpwZw',
-    title: 'Quizás, Tal vez',
-    artist: 'Eladio Carrión',
-    duration: '2:27'
-},
-{
-    src: '../music/song2.mp3',
-    img: 'https://imgs.search.brave.com/LK4SUuPdNZwxcT49dN4PODU6kCnj3Dxs1O_gqIFR7jE/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zcC1h/by5zaG9ydHBpeGVs/LmFpL2NsaWVudC9x/X2dsb3NzeSxyZXRf/aW1nLHdfMzAwL2h0/dHBzOi8vd3d3LnBv/bGFybmlnaHRzLm5l/dC93cC1jb250ZW50/L3VwbG9hZHMvMjAy/MC8xMi9iZW55LWpy/LTMwLTMwMHgzMDAu/anBn',
-    title: '8 a.m',
-    artist: 'Beny Jr',
-    duration: '3:00'
-},
-{
-    src: '../music/song3.mp3',
-    img: 'https://imgs.search.brave.com/PUqBgf0DgsOHM4GknS1K7uA_466AnxCLqe9SfWb5Adg/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pMS5z/bmRjZG4uY29tL2Fy/dHdvcmtzLUZ4anRk/MmhKelkxVi0wLXQ1/MDB4NTAwLmpwZw',
-    title: 'No estuviste en lo malo',
-    artist: 'Morad',
-    duration: '3:00'
-},
-{
-    src: '../music/song4.mp3',
-    img: 'https://imgs.search.brave.com/HozeEts6DC5xmaiwE8f9Vn1d1o9dDwbbeDi7XMqW_zo/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pMS5z/bmRjZG4uY29tL2Fy/dHdvcmtzLXZSdGd6/ZW9jYmZaczhXWTct/RDBCOVFnLXQ1MDB4/NTAwLmpwZw',
-    title: 'Columbia',
-    artist: 'Quevedo',
-    duration: '3:00'
-},
-{
-    src: '../music/song5.mp3',
-    img: 'https://imgs.search.brave.com/nAZkPX7pmfze_u8eC9nzP2MDAbhuefw0OEyCUEasthQ/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tcDN0/ZWNhLmFwcC9pbWFn/ZXMvOTcyNjBfMzUw/XzIwMjEwODIzMTYy/MjU4LmpwZw',
-    title: '23 Preguntas',
-    artist: 'Anuel AA',
-    duration: '4:11'
-},
-{
-    src: '../music/song6.mp3',
-    img: 'resources/albumBlackTho.png',
-    title: 'Ten Cuidado',
-    artist: 'Blackthoven',
-    duration: '3:00'
-},
-{
-    src: '../music/song7.mp3',
-    img: 'https://imgs.search.brave.com/lryUZe9amOE5Re8LsA1yQzPBRE2Bwe_PXL0mn7P0FQI/rs:fit:860:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvZW4vdGh1bWIv/Yy9jYS9MaWxfQmFi/eV8tX1RoZV9CaWdn/ZXJfUGljdHVyZS5w/bmcvMjIwcHgtTGls/X0JhYnlfLV9UaGVf/QmlnZ2VyX1BpY3R1/cmUucG5n',
-    title: 'The Bigger Picture',
-    artist: 'Lil Baby',
-    duration: '3:00'
-},
-{
-    src: '../music/song8.mp3',
-    img: 'https://imgs.search.brave.com/lVZQwIMDMZA8hWzgnbaT8uYEbPCduYm6xBesXVtVzGQ/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMtZXUuc3NsLWlt/YWdlcy1hbWF6b24u/Y29tL2ltYWdlcy9J/LzYxcHloSkIwWmRM/Ll9BQ19VTDIwMF9T/UjIwMCwyMDBfLmpw/Zw',
-    title: 'Black & White',
-    artist: 'Juice Wrld',
-    duration: '3:00'
-}];
-
+const songs = [
+    {
+        src: '../music/song1.mp3',
+        img: 'https://imgs.search.brave.com/4utU7dd-hTGeLy0FOv2ujN0voiRqcby1h2xuKRsvifw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pMS5z/bmRjZG4uY29tL2Fy/dHdvcmtzLTRkMmEw/Y2NmLWFkYjctNGYz/Yy1hYzg4LWE2MGFm/ZWJmMDg1Yi0wLXQ1/MDB4NTAwLmpwZw',
+        title: 'Quizás, Tal vez',
+        artist: 'Eladio Carrión',
+        duration: '2:27'
+    },
+    {
+        src: '../music/song2.mp3',
+        img: 'https://imgs.search.brave.com/LK4SUuPdNZwxcT49dN4PODU6kCnj3Dxs1O_gqIFR7jE/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zcC1h/by5zaG9ydHBpeGVs/LmFpL2NsaWVudC9x/X2dsb3NzeSxyZXRf/aW1nLHdfMzAwL2h0/dHBzOi8vd3d3LnBv/bGFybmlnaHRzLm5l/dC93cC1jb250ZW50/L3VwbG9hZHMvMjAy/MC8xMi9iZW55LWpy/LTMwLTMwMHgzMDAu/anBn',
+        title: '8 a.m',
+        artist: 'Beny Jr',
+        duration: '3:00'
+    },
+    {
+        src: '../music/song3.mp3',
+        img: 'https://imgs.search.brave.com/PUqBgf0DgsOHM4GknS1K7uA_466AnxCLqe9SfWb5Adg/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pMS5z/bmRjZG4uY29tL2Fy/dHdvcmtzLUZ4anRk/MmhKelkxVi0wLXQ1/MDB4NTAwLmpwZw',
+        title: 'No estuviste en lo malo',
+        artist: 'Morad',
+        duration: '3:00'
+    },
+    {
+        src: '../music/song4.mp3',
+        img: 'https://imgs.search.brave.com/HozeEts6DC5xmaiwE8f9Vn1d1o9dDwbbeDi7XMqW_zo/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pMS5z/bmRjZG4uY29tL2Fy/dHdvcmtzLXZSdGd6/ZW9jYmZaczhXWTct/RDBCOVFnLXQ1MDB4/NTAwLmpwZw',
+        title: 'Columbia',
+        artist: 'Quevedo',
+        duration: '3:00'
+    },
+    {
+        src: '../music/song5.mp3',
+        img: 'https://imgs.search.brave.com/nAZkPX7pmfze_u8eC9nzP2MDAbhuefw0OEyCUEasthQ/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tcDN0/ZWNhLmFwcC9pbWFn/ZXMvOTcyNjBfMzUw/XzIwMjEwODIzMTYy/MjU4LmpwZw',
+        title: '23 Preguntas',
+        artist: 'Anuel AA',
+        duration: '4:11'
+    },
+    {
+        src: '../music/song6.mp3',
+        img: 'resources/albumBlackTho.png',
+        title: 'Ten Cuidado',
+        artist: 'Blackthoven',
+        duration: '3:00'
+    },
+    {
+        src: '../music/song7.mp3',
+        img: 'https://imgs.search.brave.com/lryUZe9amOE5Re8LsA1yQzPBRE2Bwe_PXL0mn7P0FQI/rs:fit:860:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvZW4vdGh1bWIv/Yy9jYS9MaWxfQmFi/eV8tX1RoZV9CaWdn/ZXJfUGljdHVyZS5w/bmcvMjIwcHgtTGls/X0JhYnlfLV9UaGVf/QmlnZ2VyX1BpY3R1/cmUucG5n',
+        title: 'The Bigger Picture',
+        artist: 'Lil Baby',
+        duration: '3:00'
+    },
+    {
+        src: '../music/song8.mp3',
+        img: 'https://imgs.search.brave.com/lVZQwIMDMZA8hWzgnbaT8uYEbPCduYm6xBesXVtVzGQ/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMtZXUuc3NsLWlt/YWdlcy1hbWF6b24u/Y29tL2ltYWdlcy9J/LzYxcHloSkIwWmRM/Ll9BQ19VTDIwMF9T/UjIwMCwyMDBfLmpw/Zw',
+        title: 'Black & White',
+        artist: 'Juice Wrld',
+        duration: '3:00'
+    }
+];
 const radioStations = [
     {
         name: "Los 40 FM",
-        url: "http://play.rockantenne.de/rockantenne.m3u"
+        url: "http://194.169.201.177:8085/live3.mp3"
     },
     {
-        name: "Cadena 100",
-        url: ""
+        name: "Europa FM",
+        url: "http://antena3.stream.flumotion.com/antena3/europafm.mp3.m3u"
     },
     {
         name: "Radiolé",
-        url: ""
+        url: "http://22713.live.streamtheworld.com/RADIOLE.mp3"
     },
     {
         name: "Flaix FM",
@@ -96,6 +97,11 @@ const ctx = canvas.getContext("2d");
 pauseButton.style.display = "none";
 
 let sound, currentSongIndex = 0;
+let analyser, bufferLength, dataArray;
+
+// analyser = Howler.ctx.createAnalyser();
+// bufferLength = analyser.frequencyBinCount();
+// dataArray = new Uint8Array(bufferLength);
 
 //FUNCTION TO DISPLAY SONGS DATASET
 function listSongs() {
@@ -179,9 +185,11 @@ function playRadio(stationIndex) {
 
     sound = new Howl({
         src: [station.url],
+        html5: true,
         format: ['mp3'],
         autoplay: true
     });
+    sound.play();
 
     sound.on('play', function () {
         requestAnimationFrame(updateProgressBar);
@@ -202,6 +210,14 @@ function updateVolume() {
         sound.volume(newVolume);
     }
 }
+
+//FUNCTION TO LOAD THE EQUALIZER
+// function loadEqualizer() {
+//     Howler.masterGain.connect(analyser);
+//     analyser.connect(Howler.ctx.destination);
+//     analyser.fftSize = 2048;
+//     analyser.getByteTimeDomainData(dataArray);
+// }
 
 //FUNCTIONS CALLING
 listSongs();
@@ -308,4 +324,16 @@ document.getElementById("radio-station1").addEventListener("click", function () 
 
 document.getElementById("radio-station2").addEventListener("click", function () {
     playRadio(1);
+});
+
+document.getElementById("radio-station3").addEventListener("click", function () {
+    playRadio(2);
+});
+
+document.getElementById("radio-station4").addEventListener("click", function () {
+    playRadio(3);
+});
+
+document.getElementById("radio-station5").addEventListener("click", function () {
+    playRadio(4);
 });
